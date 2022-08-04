62df60d0dafe1e600af37378f1f6f92a91f03c69
bo7owers <rene7orres@gmail.com>
2022-08-03 21:56:52 -0400

    Start own reactive example

--- app.vue
+++ app.vue
@@ -1,20 +1,33 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, defineProps } from "vue";
 const state = reactive({ count: 0 });
 const increment = () => {
   state.count++;
+};
+
+const phrase = reactive({ name: "" });
+
+const sayWords = () => {
+  phrase.name = "Rene";
 };
 </script>
 <template>
   <h1>Vue basics</h1>
   <section id="reactive">
     <h2>Reacive</h2>
+    <h3>From the vue website</h3>
     <button @click="increment" class="btn btn-blue">Update count</button>
     <p aria-live="polite">Current count is {{ state.count }}</p>
+    <hr />
+    <h3>My example</h3>
+    <button @click="sayWords">Say hello</button>
+    <p>Hello {{ phrase.name }}</p>
   </section>
 </template>
 
 <style lang="sass" scoped>
+hr
+  width: 100%
 
 #reactive
   display: flex
