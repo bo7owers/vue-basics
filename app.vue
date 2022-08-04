5f63f7d8c64028def069ab6645b31a4007ecffed
bo7owers <rene7orres@gmail.com>
2022-08-03 22:19:39 -0400

    Add v-model

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { reactive, defineProps } from "vue";
+import { reactive, ref, defineProps, computed } from "vue";
 const state = reactive({ count: 0 });
 const increment = () => {
   state.count++;
@@ -10,30 +10,49 @@ const phrase = reactive({ name: "" });
 const sayWords = () => {
   phrase.name = "Rene";
 };
+
+const song = ref<string>();
+watch(song, (newName) => {
+  localStorage.setItem("myName", newName);
+});
+
+onMounted(() => {
+  song.value = localStorage.getItem("myName");
+});
 </script>
 <template>
-  <h1>Vue basics</h1>
-  <section id="reactive">
-    <h2>Reacive</h2>
-    <h3>From the vue website</h3>
-    <button @click="increment" class="btn btn-blue">Update count</button>
-    <p aria-live="polite">Current count is {{ state.count }}</p>
-    <hr />
-    <h3>My example</h3>
-    <button @click="sayWords">Say hello</button>
-    <p>Hello {{ phrase.name }}</p>
-  </section>
+  <main>
+    <h1>Vue basics</h1>
+    <section id="reactive">
+      <h2>Reacive</h2>
+      <h3>From the vue website</h3>
+      <button @click="increment" class="btn btn-blue">Update count</button>
+      <p aria-live="polite">Current count is {{ state.count }}</p>
+      <hr />
+      <h3>My example</h3>
+      <button @click="sayWords">Say hello</button>
+      <p>Hello {{ phrase.name }}</p>
+    </section>
+    <section id="v-model">
+      <h2>V-model</h2>
+      <h3>My example</h3>
+      What is your favourite song?
+      <input type="text" name="my-model" v-model="song" />
+      <p>Text will appear here {{ song }}</p>
+    </section>
+  </main>
 </template>
 
 <style lang="sass" scoped>
 hr
   width: 100%
 
-#reactive
+main
   display: flex
   flex-flow: column wrap
   align-items: center
 
+#reactive
   .btn
     margin-inline: auto
     height: 1.75rem
